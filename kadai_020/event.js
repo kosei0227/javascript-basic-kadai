eb31f8c4cc43a1c9e5d75b166ce40560aa4612a9
kosei0227 <167411842+kosei0227@users.noreply.github.com>
2024-08-04 06:41:42 +0900

    kadai_020を提出

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const btn2 = document.getElementById('btn');
+
+btn2.addEventListener('click', () => {
+    const text2 = document.getElementById('text');
+    text2.textContent = 'ボタンがクリックされました';
+});
